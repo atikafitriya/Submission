5d7ca7008207115b5d137a4557640a1d46869fc7
[firstname <atika.habibatulf@gmail.com>
2024-11-15 10:13:13 +0700

    first commit

---
+++ Dashboard/Dashboard.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
+
+all_df = pd.read_csv("all_df.csv")
+
+st.header('Dicoding Collection Dashboard :sparkles:')
